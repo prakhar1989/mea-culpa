e35865d994020da4f6166d96787ab040bdacef50
Prakhar Srivastav <prakhar1989@gmail.com>
2015-07-20 16:04:05 +0300

    Fixed /courses route and enabled CORS

--- server/app.js
+++ server/app.js
@@ -2,6 +2,13 @@ var express = require('express');
 var app = express();
 var Api = require('./api');
 
+// allow CORS
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 app.get('/', function(req, res) {
     res.send(JSON.stringify({msg: "mea culpa!"}));
 });
